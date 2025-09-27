258dd94ec821edcf0c6991aed9b38bad9bb49632
gxsilva <lssvgabriel@gmail.com>
2025-09-26 22:01:10 -0300

    sep26

--- search_insert_position/main.cpp
+++ search_insert_position/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,7 +12,7 @@ class Solution {
 public:
 
 	//=======================O(N)====================================
-	static int searchInsertIterator(vector<int>& nums, int target) {
+	int searchInsertIterator(vector<int>& nums, int target) {
 		vector<int>::iterator it;
 		int index = 0;
 		for (it = nums.begin(); it != nums.end(); it++, index++)
@@ -25,7 +29,7 @@ public:
 		return (index);
 	}
 	//way more faster acesses an index directly than look an pointer, =~1/7 more faster
-	static int searchInsertIndex(vector<int>& nums, int target) {
+	int searchInsertIndex(vector<int>& nums, int target) {
 		int i;
 		for (i = 0; i < nums.size(); i++)
 		{
@@ -37,9 +41,9 @@ public:
 		return (i);
 	}
 	//=======================O(Log N)====================================
-	static int searchInsertBinarySearch(vector<int>& nums, int target) {
+	int searchInsertBinarySearch(vector<int>& nums, int target) {
 		int left = 0;
-		int right = nums.size();
+		int right = nums.size() - 1;
 		while (left <= right)
 		{
 			int mid = left + (right - left) / 2;
@@ -50,16 +54,36 @@ public:
 		}
 		return (left);
 	}
+
+	//=======================UTILS====================================
+	static vector<int> &randomIntVector(int vSize, int min, int max) {
+		vector<int> *v = new vector<int>(vSize);
+		for (int i = 0; i < vSize; i++)
+			(*v)[i] = 1 + (rand() % (max - min + 1));
+		sort(v->begin(), v->end());
+		auto it = unique(v->begin(), v->end());
+		v->resize(distance(v->begin(), it));
+		return (*v);
+	}
+
+	int randomNumber(int range)
+	{
+		return ((1 + (rand() & (range - 1  + 1))));
+	}
 };
 
 int main(void)
 {
-	vector<int> v = {1,3,5,6};
+	int	range =  1000;
+	srand(static_cast<unsigned int>(time(0)));
+	Solution s = Solution();
+	vector<int> v = Solution::randomIntVector(range, 1, 1000);
+	cout << "Vector size: " << v.size() << endl;
 	{
 		auto start_time = std::chrono::high_resolution_clock::now();
-		cout << "res: " << Solution::searchInsertIterator(v, 5) << endl; // 2
-		cout << "res: " << Solution::searchInsertIterator(v, 2) << endl; // 1
-		cout << "res: " << Solution::searchInsertIterator(v, 7) << endl; // 4
+		cout << "res: " << s.searchInsertIterator(v, v[s.randomNumber(range)]) << endl; // 2
+		cout << "res: " << s.searchInsertIterator(v, v[s.randomNumber(range)]) << endl; // 1
+		cout << "res: " << s.searchInsertIterator(v, v[s.randomNumber(range)]) << endl; // 4
 		auto end_time = std::chrono::high_resolution_clock::now();
 		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
 		cout << "Time the function has taken: " << duration.count() << endl;
@@ -67,9 +91,9 @@ int main(void)
 	cout << endl;
 	{
 		auto start_time = std::chrono::high_resolution_clock::now();
-		cout << "res: " << Solution::searchInsertIndex(v, 5) << endl; // 2
-		cout << "res: " << Solution::searchInsertIndex(v, 2) << endl; // 1
-		cout << "res: " << Solution::searchInsertIndex(v, 7) << endl; // 4
+		cout << "res: " << s.searchInsertIndex(v, v[s.randomNumber(range)]) << endl; // 2
+		cout << "res: " << s.searchInsertIndex(v, v[s.randomNumber(range)]) << endl; // 1
+		cout << "res: " << s.searchInsertIndex(v, v[s.randomNumber(range)]) << endl; // 4
 		auto end_time = std::chrono::high_resolution_clock::now();
 		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
 		cout << "Time the function has taken: " << duration.count() << endl;
@@ -77,14 +101,17 @@ int main(void)
 	cout << endl;
 	{
 		auto start_time = std::chrono::high_resolution_clock::now();
-		cout << "res: " << Solution::searchInsertBinarySearch(v, 5) << endl; // 2
-		cout << "res: " << Solution::searchInsertBinarySearch(v, 2) << endl; // 1
-		cout << "res: " << Solution::searchInsertBinarySearch(v, 7) << endl; // 4
+		cout << "res: " << s.searchInsertBinarySearch(v, v[s.randomNumber(range)]) << endl; // 2
+		cout << "res: " << s.searchInsertBinarySearch(v, v[s.randomNumber(range)]) << endl; // 1
+		cout << "res: " << s.searchInsertBinarySearch(v, v[s.randomNumber(range)]) << endl; // 4
 		auto end_time = std::chrono::high_resolution_clock::now();
 		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
 		cout << "Time the function has taken: " << duration.count() << endl;
 	}
-	int half_middle = 0 + (9 - 0) / 2;
-	cout << "mid: " << half_middle << endl;
+	cout << endl;
+	{
+		vector<int> v2 = {15, 30, 45, 60};
+		cout << "size: " << v2.size() << endl;
+	}
 	return (0);
 }
